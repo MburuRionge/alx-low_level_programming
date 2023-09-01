3b023578e835f7c9c8071979ab5111c4897bc57c
MburuRionge <victormburu3@gmail.com>
2023-09-01 16:17:33 -0700

    this program prints the multiplication of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,56 @@
 #include <stdio.h>
 
 /**
+ * _atoi - checks if the arguments are integers
+ * @t: pointer to a variable
+ * Return: (n)
+ */
+
+int _atoi(char *t)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (i < len && f == 0)
+	{
+		if (t[i] == '-')
+		{
+			++d;
+		}
+
+		if (t[i] >= '0' && t[i] <= '9')
+		{
+			digit = t[i] - '0';
+
+			if (d % 2)
+			{
+				digit = -digit;
+			}
+			n = n * 10 + digit;
+
+			f = i;
+
+			if (t[i + 1] < '0' || t[i + 1] > '9')
+			{
+				break;
+			}
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+	{
+		return (0);
+	}
+	return (n);
+}
+/**
  * main - provide two arguments
  * @argc: represents the number of arguments
  * @argv: its an array of strings that stores the number of arguments
@@ -10,21 +60,18 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int product, num1, num2;
+
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		int a, b, product;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
+	product = num1 * num2;
+	printf("%d\n", product);
 
-		product = a * b;
-
-		printf("The product of %d and %d is %d\n", a, b, product);
-	}
 	return (0);
 }
