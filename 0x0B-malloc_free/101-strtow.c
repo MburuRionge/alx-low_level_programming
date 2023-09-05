93e7c67d20d88cef8944c90c64cdf5d295a44ab8
MburuRionge <victormburu3@gmail.com>
2023-09-04 19:08:29 -0700

    a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,9 +1,85 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * strtow -
- * @str: a pointer
- * Return:
+ * count_word - counts number of words in a string
+ * *t: string to evaluate
+ * Return: number of words
+ */
+int count_word(char *t)
+{
+	int flag, n, p;
+	
+	flag = 0;
+	p = 0;
+	
+	for (n = 0; t[n] != '\0'; n++)
+	{
+		if (t[n] == ' ')
+		{
+			flag = 0;
+		}
+		else if (flag == 0)
+		{
+			flag = 1;
+			p++;
+		}
+	}
+
+	return (p);
+}
+
+/**
+ * strtow - splits the input into multiple substrings
+ * @str: a string input
+ * Return: an array os strings (success)
+ * or NULL (Error)
  */
 
 char **strtow(char *str)
+{
+	char **space, *ptr;
+	int v, m = 0, len = 0, words, n = 0, start, end;
+
+	while (*str + len)
+		len++;
+	words = count_word(str);
+	if (words == 0)
+	{
+		return (NULL);
+	}
+
+	len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	space = (char **) malloc(sizeof(char *) * (words + 1));
+	if (space == NULL)
+		return (NULL);
+
+	for (v = 0; v <= len; v++)
+	{
+		if (str[v] == ' ' || str[v] == '\0')
+		{
+			if (n)
+			{
+				end = v;
+				ptr = (char *) malloc(sizeof(char) * (n + 1));
+
+				if (ptr == NULL)
+					return (NULL);
+				while (start < end)
+					*ptr++ = str[start++];
+				*ptr = '\0';
+				space[m] = ptr - n;
+				m++;
+				n = 0;
+			}
+		}
+		else if (n++ == 0)
+			start = v;
+	}
+	space[m] = NULL;
+
+	return (space);
+}
