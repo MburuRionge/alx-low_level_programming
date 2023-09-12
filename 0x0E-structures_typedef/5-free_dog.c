06cb1c329343df7c0298db504d5d811e9c34cefc
MburuRionge <victormburu3@gmail.com>
2023-09-11 20:00:20 -0700

    freeing

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * main - check code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	init_dog(&my_dog, "poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.lf :) - woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
