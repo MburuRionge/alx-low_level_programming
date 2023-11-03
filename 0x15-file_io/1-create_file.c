8f45c40e3a32f83901184bf5893b19a9c7538c7d
Mburu Rionge <victormburu3@gmail.com>
2023-11-03 06:29:00 -0700

    updating the errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,9 @@ int create_file(const char *filename, char *text_content)
 	int tr;
 	int wr;
 
+	if (!filename)
+		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
@@ -26,6 +29,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (wr == -1)
 		return (-1);
+
 	close(fd);
+
 	return (1);
 }
